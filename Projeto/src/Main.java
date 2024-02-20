e4964107fbb4c9e8eeda00bdbb9f3893e88eb837
Pedroinatel <159315171+Pedroinatel@users.noreply.github.com>
2024-02-20 14:07:30 -0300

    Código "hello java" código da primeira aula

---
+++ Projeto/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+       // int x;
+       // int y;
+
+        System.out.println("Hello Word!!");
+      // System.out.println(x + y);
+       // System.out.println("A soma de x e y é:"+ (x + y));  //O primeiro + é para concat...
+
+    }
+
+
+}
